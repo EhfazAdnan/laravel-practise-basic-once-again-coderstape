c077cb17f5db48e49582c3a671f8d2dbd12dd320
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-11-30 18:59:21 +0600

    Install laravel 5.8 and create contact and about routes

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('contact', function(){
+    return 'Contact Us';
+});
+
+Route::get('about', function(){
+    return 'About Us';
+});
