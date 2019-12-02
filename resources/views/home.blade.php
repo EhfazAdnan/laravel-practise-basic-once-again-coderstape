0eeecac81d3e886ced0646b3e654eb312e403367
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-02 23:03:32 +0600

    Vue basic button and fetch data from database with axios|Api

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <my-button text="My New Text Button" type="submit"></my-button>
                 </div>
             </div>
         </div>
